667dbdb29cfdbd63757e2746abe262ca9c5bf91a
slavuha <kergakovslava@gmail.com>
2021-08-03 17:42:59 +0300

    delete comments

--- app/src/main/java/com/example/pokemonsinfo/network/PokemonsAPI.kt
+++ app/src/main/java/com/example/pokemonsinfo/network/PokemonsAPI.kt
@@ -9,40 +9,20 @@ import retrofit2.http.GET
 
 private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
-/**
- * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
- * full Kotlin compatibility.
- */
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
-/**
- * Use the Retrofit builder to build a retrofit obj ect using a Moshi converter with our Moshi
- * object.
- */
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
-/**
- * A public interface that exposes the [getProperties] method
- */
 interface PokemonApiService {
-    /**
-     * Returns a Coroutine [List] of [MarsProperty] which can be fetched with await() if
-     * in a Coroutine scope.
-     * The @GET annotation indicates that the "realestate" endpoint will be requested with the GET
-     * HTTP method
-     */
     @GET("pokemon/")
     suspend fun getProperties(): Pokemon
 }
 
-/**
- * A public Api object that exposes the lazy-initialized Retrofit service
- */
 object PokemonApi {
     val RETROFIT_SERVICE: PokemonApiService by lazy { retrofit.create(PokemonApiService::class.java) }
 }
